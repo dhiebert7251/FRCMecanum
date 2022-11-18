c2d4d46401444f959d49da25cab16195a62b00f9
dhiebert7251 <dan.hiebert@sbcglobal.net>
2022-11-18 12:02:29 -0600

    robot container updated

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -4,12 +4,22 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drivetrain;
 
 public class DriveWithJoysticks extends CommandBase {
+  private final Drivetrain m_driveTrain;
+  private final XboxController m_driverJoystick;
+
+  
   /** Creates a new DriveWithJoysticks. */
-  public DriveWithJoysticks() {
+  public DriveWithJoysticks(Drivetrain driveTrain, XboxController driverJoystick) {
+    m_driveTrain = driveTrain;
+    m_driverJoystick = driverJoystick;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_driveTrain);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +28,21 @@ public class DriveWithJoysticks extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    double ySpeed = m_driverJoystick.getLeftY()*Constants.DRIVETRAIN_SPEED;
+    double xSpeed = m_driverJoystick.getLeftX()*Constants.DRIVETRAIN_SPEED;
+    double zRotation = m_driverJoystick.getRightX();  //add rotation scale constant?
+
+    m_driveTrain.driveWithJoysticks(ySpeed, xSpeed, zRotation);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    m_driveTrain.driveWithJoysticks(0, 0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
