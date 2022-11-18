a5903c4c16815697b9d6224abc124f3ede27f1ce
dhiebert7251 <dan.hiebert@sbcglobal.net>
2022-11-18 11:17:44 -0600

    drivertrain subsystem added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,34 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+  //Motor constants
+  public static final int LEFT_FRONT_MOTOR = 0;
+  public static final int LEFT_REAR_MOTOR = 1;
+  public static final int RIGHT_FRONT_MOTOR = 2;
+  public static final int RIGHT_REAR_MOTOR = 3;
+
+  //Controller constants
+
+  //controller - Driver
+  public static final int DRIVER_JOYSTICK = 0;
+
+  //controller - Driver Axis maps
+  public static final int DRIVER_JOYSTICK_LEFT_X_AXIS = 0;
+  public static final int DRIVER_JOYSTICK_LEFT_Y_AXIS = 1;
+  public static final int DRIVER_JOYSTICK_RIGHT_X_AXIS = 2;
+  public static final int DRIVER_JOYSTICK_RIGHT_Y_AXIS = 3;
+
+  //controller - Driver Button maps
+  public static final int DRIVER_LEFT = 1;
+  public static final int DRIVER_RIGHT = 3;
+  public static final int DRIVER_UP = 4;
+  public static final int DRIVER_DOWN = 2;
+  public static final int DRIVER_SHOULDER_TOP_LEFT = 5;
+  public static final int DRIVER_SHOULDER_TOP_RIGHT = 6;
+  public static final int DRIVER_SHOULDER_BOTTOM_LEFT = 7;
+  public static final int DRIVER_SHOULDER_BOTTOM_RIGHT = 8;
+  public static final int DRIVER_LEFT_JOYSTICK = 9;
+  public static final int DRIVER_RIGHT_JOYSTICK = 10;
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+public class Drivetrain extends SubsystemBase {
+
+
+  //drivetrain speed controllers    
+  Spark leftFrontMotor;
+  Spark rightFrontMotor;
+  Spark leftRearMotor;
+  Spark rightRearMotor;
+
+  //drivetrain type
+  MecanumDrive drive;
+  
+    /** Creates a new Drivetrain. */
+    public Drivetrain() {
+      leftFrontMotor = new Spark(Constants.LEFT_FRONT_MOTOR);
+      leftFrontMotor.setInverted(false);
+      leftRearMotor = new Spark(Constants.LEFT_REAR_MOTOR);
+      leftRearMotor.setInverted(false);
+      rightFrontMotor = new Spark(Constants.RIGHT_FRONT_MOTOR);
+      rightFrontMotor.setInverted(false);
+      rightRearMotor = new Spark(Constants.RIGHT_REAR_MOTOR);
+      rightRearMotor.setInverted(false);
+    
+      drive = new MecanumDrive(leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor);
+      
+ 
+    }
+  
+    public void driveWithJoysticks(double ySpeed, double xSpeed, double zRotation)
+    {
+      drive.driveCartesian(ySpeed, xSpeed, zRotation);
+    }
+
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+  
+    public void stop() {
+      drive.stopMotor();
+    }
+}
